e55f11e3ad4879a74d7805af8c1b92a90eba3577
Bhushan Bahalkar <bhushanbahalkar189@gmail.com>
2024-05-08 13:24:15 +0530

    commit changes

--- LinkedListMiddleElement.java
+++ LinkedListMiddleElement.java
@@ -28,14 +28,14 @@ public class LinkedListMiddleElement extends Node {
             System.out.println("List is empty");
             return;
         }
-        int tempp = 0;
+        int count = 0;
         Node temp = head;
         while (temp != null) {
-            tempp++;
+            count++;
             temp = temp.next;
         }
         temp = head;
-        for (int i = 0; i < tempp / 2; i++) {
+        for (int i = 0; i < count / 2; i++) {
             temp = temp.next;
         }
         System.out.println("Middle element: " + temp.name);
@@ -49,10 +49,32 @@ public class LinkedListMiddleElement extends Node {
         }
     }
 
-//    public void deletemiddlenode(){
-//        int lengthofLL = getlength();
-//        int getMiddle = lengthofLL/2
-//    }
+
+    public int getlength(){
+        Node temp =head;
+        int count =0;
+        while(temp!= null){
+            temp = temp.next;
+            count++;
+        }
+        System.out.println(count);
+        return count;
+
+    }
+    public void deletemiddlenode(){
+        int lengthofLL = getlength();
+        int getMiddle = (lengthofLL / 2) - 1;
+
+        Node temp = head;
+        while(getMiddle > 0){
+            temp = temp.next;
+            getMiddle--;
+        }
+
+        Node temp2 = temp.next;
+        temp.next = temp2.next;
+        temp2.next = null;
+    }
 
 
 
@@ -71,5 +93,8 @@ public class LinkedListMiddleElement extends Node {
 
         list.printMyLinkedList();
         list.middleElement();
+        System.out.println("After deleting middle element:");
+        list.deletemiddlenode();
+        list.printMyLinkedList();
     }
 }
