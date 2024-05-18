a27e7dc657c83adee997d40f8659f403e6a7be68
Bhushan Bahalkar <bhushanbahalkar189@gmail.com>
2024-05-18 23:05:33 +0530

    commit changes

--- LinkedListMiddleElement.java
+++ LinkedListMiddleElement.java
@@ -29,12 +29,12 @@ public class LinkedListMiddleElement extends Node {
             return;
         }
         int count = 0;
-//        Node temp = head;
-//        while (temp != null) {
-//            count++;
-//            temp = temp.next;
-//        }
-//        temp = head;
+        Node temp = head;
+        while (temp != null) {
+            count++;
+            temp = temp.next;
+        }
+        temp = head;
         for (int i = 0; i < count / 2; i++) {
             temp = temp.next;
         }
