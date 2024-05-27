aea94d0b62f9b3976b0d50bff0cac5e432e1f300
Bhushan Bahalkar <bhushanbahalkar189@gmail.com>
2024-05-27 14:20:04 +0530

    reverted to original

--- StringFreq.java
+++ StringFreq.java
@@ -11,18 +11,18 @@ public class StringFreq{
         System.out.println("Enter Your String");
         String str = br.readLine();
         System.out.println("Input String is " + str);
-//
-//        int tempA = 0;
-//        int tempB = 0;
-//
-//        for (int i = 0; i < str.length(); i++) {
-//            char ch = str.charAt(i);
-//            if (ch == 'A') {
-//                tempA++;
-//            } else if (ch == 'I') {
-//               tempB++;
-//            }
-//        }
+
+        int tempA = 0;
+        int tempB = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == 'A') {
+                tempA++;
+            } else if (ch == 'I') {
+               tempB++;
+            }
+        }
 
         System.out.println("Frequency of 'A': " + tempA);
         System.out.println("Frequency of 'I': " + tempB);
