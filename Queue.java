509ecf4fdc89678e78ade3bd75947ea120b8f688
Bhushan Bahalkar <bhushanbahalkar189@gmail.com>
2024-05-31 14:17:05 +0530

    new file added

--- Queue.java
+++ Queue.java
@@ -9,13 +9,13 @@ public class Queue {
        start++;
     }
 
-//    public void dequeue(int[] arr) {
-//        arr[end] = 0;
-//        int Element = arr[end];
-//        System.out.println("Element: " + arr[end]);
-//        arr[end] = 0;
-//        end++;
-//    }
+    public void dequeue(int[] arr) {
+        arr[end] = 0;
+        int Element = arr[end];
+        System.out.println("Element: " + arr[end]);
+        arr[end] = 0;
+        end++;
+    }
 
     public void printQueue(int[] arr){
         for(int i =end; i< start; i++){
