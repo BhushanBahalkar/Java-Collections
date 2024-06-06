871936729a1d38c1191a1c7c190f81fd2978e3ae
Bhushan Bahalkar <bhushanbahalkar189@gmail.com>
2024-06-06 10:54:40 +0530

    commited changes

--- MyList.java
+++ MyList.java
@@ -70,25 +70,13 @@ class Main {
         myList.add(20);
         myList.add(30);
         myList.add(40);
-        myList.add(50);
-        myList.add(60);
-        myList.add(10);
+
         myList.add(20);
         myList.add(30);
         myList.add(40);
         myList.add(50);
         myList.add(60);
-        myList.add(10);
-        myList.add(20);
-        myList.add(30);
-        myList.add(40);
-        myList.add(50);
-        myList.add(60);
-        myList.add(10);
-        myList.add(20);
-        myList.add(30);
-        myList.add(40);
-        myList.add(50);
+
         myList.add(60);
     }
 }
